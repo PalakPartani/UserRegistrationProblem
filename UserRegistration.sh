605660856acf9ed586284d4883186416cdba2a57
PalakPartani <partanipalak16@gmail.com>
2020-02-14 17:43:01 +0530

    ADDED:function to validate lastname

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,13 +2,13 @@
 echo "Welcome to user registration problem "
 userNamePattern="[A-Z]{1}[A-Za-z]{2,}"
 echo "Enter UserName:"
-read firstName 
+read firstName lastName
 function checkUserDetails(){
-	if [[ $1 =~ $2 ]]
+	if [[ $1 =~ $3 && $2 =~ $3 ]]
 	then
 		echo "Valid username"
 	else
 		echo "Invalid username"
 	fi
 }
-checkUserDetails $firstName $userNamePattern
+checkUserDetails $firstName $lastName $userNamePattern
