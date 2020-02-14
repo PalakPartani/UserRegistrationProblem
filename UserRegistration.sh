a34a5350056a33bf4f081ee3070c4f4bd9920e36
PalakPartani <partanipalak16@gmail.com>
2020-02-14 11:04:33 +0530

    ADDED:pattern to validate first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to user registration problem "
-
+echo "Enter UserName:"
+read userName
+userNamePattern="[A-Z]{1}[A-Za-z]{2,}"
+if [[ $userName =~ $userNamePattern ]]
+then
+	echo "Valid username"
+else
+	echo "Invalid username"
+fi
