59c1e6c424064d8507188be1b22e70ab28875837
PalakPartani <partanipalak16@gmail.com>
2020-02-17 09:42:20 +0530

    ADDED:feature to validate atleast one uppercase character

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -3,7 +3,7 @@ echo "Welcome to user registration problem "
 userNamePattern="[A-Z]{1}[A-Za-z]{2,}"
 emailIdPattern="^[A-Za-z]*([.|+|-|_]?[A-Za-z]+)?[@]{1}[A-Za-z]{2,}[.]{1}[A-Za-z]{2,}([.]?[A-Za-z]{2,})?$"
 checkNumber="^[1-9]{2}\s?[1-9]{1}[0-9]{9}$"
-checkPassword="^[A-Za-z]{8}"
+checkPassword="^[A-Z]{1,}[A-Za-z]{8,}$"
 
 #function to check validation
 function checkUserDetails(){
@@ -15,17 +15,17 @@ function checkUserDetails(){
 		echo "Invalid!"
 	fi
 }
-echo "Enter Username:"
-read firstName
-checkUserDetails $firstName $userNamePattern
-read lastName
-checkUserDetails $lastName $userNamePattern
-echo "Enter Emailid :"
-read emailId
-checkUserDetails $emailId $emailIdPattern 
-echo "Enter mobile no preceeding country code "
-read number
-checkUserDetails $number $checkNumber 
+#echo "Enter Username:"
+#read firstName
+#checkUserDetails $firstName $userNamePattern
+#read lastName
+#checkUserDetails $lastName $userNamePattern
+#echo "Enter Emailid :"
+#read emailId
+#checkUserDetails $emailId $emailIdPattern 
+3echo "Enter mobile no preceeding country code "
+#read number
+#checkUserDetails $number $checkNumber 
 echo "enter Password :"
 read password
 checkUserDetails $password $checkPassword
